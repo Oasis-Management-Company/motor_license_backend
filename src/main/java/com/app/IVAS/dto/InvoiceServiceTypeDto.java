30e3c3d445338629c6afdd14f3be916208c82cd0
Busola <busoladipo@gmail.com>
2023-03-30 06:34:13 +0100

    update

---
+++ src/main/java/com/app/IVAS/dto/InvoiceServiceTypeDto.java
@@ -0,0 +1,15 @@
+package com.app.IVAS.dto;
+
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class InvoiceServiceTypeDto extends ServiceTypeDto {
+
+    String vehicleCategory;
+    String reference;
+    LocalDateTime paymentDate;
+    LocalDateTime expiryDate;
+
+}
--- src/main/java/com/app/IVAS/repository/InvoiceServiceTypeRepository.java
+++ src/main/java/com/app/IVAS/repository/InvoiceServiceTypeRepository.java
@@ -1,9 +1,15 @@
 package com.app.IVAS.repository;
 
+import com.app.IVAS.entity.Invoice;
 import com.app.IVAS.entity.InvoiceServiceType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface InvoiceServiceTypeRepository extends JpaRepository<InvoiceServiceType, Long> {
+
+    Optional<List<InvoiceServiceType>> findAllByInvoice(Invoice invoice);
 }
