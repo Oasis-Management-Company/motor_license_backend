a7c88ad7333421af008ebad4f08b4d0a6917429b
chinomso uhuegbulem <>
2023-04-10 20:44:39 +0100

    updates

--- src/main/java/com/app/IVAS/dto/filters/PlateNumberRequestSearchFilter.java
+++ src/main/java/com/app/IVAS/dto/filters/PlateNumberRequestSearchFilter.java
@@ -21,8 +21,11 @@ public class PlateNumberRequestSearchFilter extends BaseSearchDto implements Que
         bindings.bind(root.assignmentStatus).as("assigmentStatus").first((path, value) -> path.eq(value));
         bindings.bind(root.workFlowApprovalStatus).as("workFlowStatus").first((path, value) -> path.eq(value));
         bindings.bind(root.createdBy.displayName).as("name").first((path, value) -> path.containsIgnoreCase(value));
-        bindings.bind(root.workFlow.stage.ApprovingOfficer.displayName).as("approvingOfficer").first((path, value) -> path.containsIgnoreCase(value));
+        bindings.bind(root.createdBy.office.zone.id).as("zone").first((path, value) -> path.eq(value));
+        if (root.workFlow.stage.ApprovingOfficer != null){
+            bindings.bind(root.workFlow.stage.ApprovingOfficer.displayName).as("approvingOfficer").first((path, value) -> path.containsIgnoreCase(value));
+        }
         bindings.bind(root.plateNumberType.id).as("plateNumberTYpe").first((path, value) -> path.eq(value));
-        bindings.including(root.trackingId, root.assignmentStatus, root.workFlow.stage.ApprovingOfficer, root.createdBy, root.plateNumberType, root.workFlowApprovalStatus);
+        bindings.including(root.trackingId, root.assignmentStatus, root.workFlow.stage, root.createdBy, root.plateNumberType, root.workFlowApprovalStatus);
     }
 }
