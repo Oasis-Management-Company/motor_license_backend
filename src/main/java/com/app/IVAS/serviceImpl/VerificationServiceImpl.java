9d88e9f7ee7b967a9ccd39a41f4e2ca8254f3da9
Busola <busoladipo@gmail.com>
2023-04-12 17:01:31 +0100

    update

--- src/main/java/com/app/IVAS/serviceImpl/VerificationServiceImpl.java
+++ src/main/java/com/app/IVAS/serviceImpl/VerificationServiceImpl.java
@@ -36,121 +36,124 @@ public class VerificationServiceImpl implements VerificationService {
         Optional<Invoice> invoice = invoiceRepository.findByInvoiceNumberIgnoreCase(invoiceNumber);
         if (invoice.isPresent()) {
 
-            Optional<Vehicle> vehicle = vehicleRepository.findById(invoice.get().getVehicle().getId());
+            if (invoice.get().getVehicle() != null) {
 
-            verificationDto.setInvoiceNumber(invoice.get().getInvoiceNumber());
-            verificationDto.setPaymentDate(invoice.get().getPaymentDate());
-            verificationDto.setAmount(invoice.get().getAmount());
-            verificationDto.setPaymentStatus(invoice.get().getPaymentStatus().name());
-            if(invoice.get().getPaymentDate() != null) {
-                verificationDto.setExpiryDate(invoice.get().getPaymentDate().plusYears(1));
-            }
+                Optional<Vehicle> vehicle = vehicleRepository.findById(invoice.get().getVehicle().getId());
 
-
-            PortalUser portalUser = portalUserRepository.findById(invoice.get().getPayer().getId()).get();
-
-            if (portalUser.getDisplayName() != null) {
-                verificationDto.setName(portalUser.getDisplayName());
-            }
-
-            if (portalUser.getPhoneNumber() != null) {
-                verificationDto.setPhoneNumber(portalUser.getPhoneNumber());
-            }
-
-            if (portalUser.getAddress() != null) {
-                verificationDto.setAddress(portalUser.getAddress());
-            }
-
-            if (portalUser.getEmail() != null) {
-                verificationDto.setEmail(portalUser.getEmail());
-            }
-
-            if (portalUser.getAsin() != null) {
-                verificationDto.setAsin(portalUser.getAsin());
-            }
-
-            if(vehicle.isPresent()){
-                if(vehicle.get().getChasisNumber() != null){
-                    verificationDto.setChasis(vehicle.get().getChasisNumber());
-                }
-                if(vehicle.get().getEngineNumber()!= null){
-                    verificationDto.setEngine(vehicle.get().getEngineNumber());
-                }
-                if(vehicle.get().getVehicleCategory() != null){
-                    verificationDto.setCategory(vehicle.get().getVehicleCategory().getName());
-                }
-                if(vehicle.get().getVehicleCategory().getWeight() != null){
-                    verificationDto.setWeight(vehicle.get().getVehicleCategory().getWeight());
-                }
-                if(vehicle.get().getVehicleModel() != null){
-                    verificationDto.setModel(vehicle.get().getVehicleModel().getName());
-                }
-
-                if(vehicle.get().getVehicleModel().getVehicleMake().getName() != null){
-                    verificationDto.setMake(vehicle.get().getVehicleModel().getVehicleMake().getName());
-                }
-
-                if(vehicle.get().getVehicleModel().getYear()!= null){
-                    verificationDto.setYear(vehicle.get().getVehicleModel().getYear());
-                }
-
-                if(vehicle.get().getColor()!= null){
-                    verificationDto.setColor(vehicle.get().getColor());
-                }
-
-                if(vehicle.get().getPlateNumber().getPlateNumber()!= null){
-                    verificationDto.setPlate(vehicle.get().getPlateNumber().getPlateNumber().toUpperCase());
+                verificationDto.setInvoiceNumber(invoice.get().getInvoiceNumber());
+                verificationDto.setPaymentDate(invoice.get().getPaymentDate());
+                verificationDto.setAmount(invoice.get().getAmount());
+                verificationDto.setPaymentStatus(invoice.get().getPaymentStatus().name());
+                if (invoice.get().getPaymentDate() != null) {
+                    verificationDto.setExpiryDate(invoice.get().getPaymentDate().plusYears(1));
                 }
 
 
-            }
+                PortalUser portalUser = portalUserRepository.findById(invoice.get().getPayer().getId()).get();
 
+                if (portalUser.getDisplayName() != null) {
+                    verificationDto.setName(portalUser.getDisplayName());
+                }
 
-            Optional<List<InvoiceServiceType>> invoiceServiceTypes = invoiceServiceTypeRepository.findAllByInvoice(invoice.get());
+                if (portalUser.getPhoneNumber() != null) {
+                    verificationDto.setPhoneNumber(portalUser.getPhoneNumber());
+                }
 
-            List<InvoiceServiceTypeDto> invoiceServiceTypeDtos = new ArrayList<>();
+                if (portalUser.getAddress() != null) {
+                    verificationDto.setAddress(portalUser.getAddress());
+                }
 
-            if (invoiceServiceTypes.isPresent()) {
-                for (InvoiceServiceType invoiceServiceType : invoiceServiceTypes.get()) {
+                if (portalUser.getEmail() != null) {
+                    verificationDto.setEmail(portalUser.getEmail());
+                }
 
-                    InvoiceServiceTypeDto invoiceServiceTypeDto = new InvoiceServiceTypeDto();
+                if (portalUser.getAsin() != null) {
+                    verificationDto.setAsin(portalUser.getAsin());
+                }
 
-                    if(invoiceServiceType.getServiceType().getName() != null) {
-                        invoiceServiceTypeDto.setName(invoiceServiceType.getServiceType().getName());
+                if (vehicle.isPresent()) {
+                    if (vehicle.get().getChasisNumber() != null) {
+                        verificationDto.setChasis(vehicle.get().getChasisNumber());
+                    }
+                    if (vehicle.get().getEngineNumber() != null) {
+                        verificationDto.setEngine(vehicle.get().getEngineNumber());
+                    }
+                    if (vehicle.get().getVehicleCategory() != null) {
+                        verificationDto.setCategory(vehicle.get().getVehicleCategory().getName());
+                    }
+                    if (vehicle.get().getVehicleCategory().getWeight() != null) {
+                        verificationDto.setWeight(vehicle.get().getVehicleCategory().getWeight());
+                    }
+                    if (vehicle.get().getVehicleModel() != null) {
+                        verificationDto.setModel(vehicle.get().getVehicleModel().getName());
                     }
 
-                    if(invoiceServiceType.getServiceType().getPrice() != null) {
-                        invoiceServiceTypeDto.setPrice(invoiceServiceType.getServiceType().getPrice());
+                    if (vehicle.get().getVehicleModel().getVehicleMake().getName() != null) {
+                        verificationDto.setMake(vehicle.get().getVehicleModel().getVehicleMake().getName());
                     }
 
-                    if(invoiceServiceType.getServiceType().getDurationInMonth() != null) {
-                        invoiceServiceTypeDto.setDurationInMonth(invoiceServiceType.getServiceType().getDurationInMonth());
+                    if (vehicle.get().getVehicleModel().getYear() != null) {
+                        verificationDto.setYear(vehicle.get().getVehicleModel().getYear());
                     }
 
-                    if(invoiceServiceType.getServiceType().getCategory().getName() != null) {
-                        invoiceServiceTypeDto.setVehicleCategory(invoiceServiceType.getServiceType().getCategory().getName());
+                    if (vehicle.get().getColor() != null) {
+                        verificationDto.setColor(vehicle.get().getColor());
                     }
 
-                    if(invoiceServiceType.getReference() != null) {
-                        invoiceServiceTypeDto.setReference(invoiceServiceType.getReference());
+                    if (vehicle.get().getPlateNumber().getPlateNumber() != null) {
+                        verificationDto.setPlate(vehicle.get().getPlateNumber().getPlateNumber().toUpperCase());
                     }
 
-                    if(invoiceServiceType.getPaymentDate() != null) {
-                        invoiceServiceTypeDto.setPaymentDate(invoiceServiceType.getPaymentDate());
-                    }
 
-                    if(invoiceServiceType.getServiceType().getDurationInMonth() != null && invoiceServiceType.getPaymentDate() != null){
-                        invoiceServiceTypeDto.setExpiryDate(invoiceServiceType.getPaymentDate().plusMonths(invoiceServiceType.getServiceType().getDurationInMonth()));
-                    }
-
-                    invoiceServiceTypeDtos.add(invoiceServiceTypeDto);
                 }
 
-                verificationDto.setInvoiceServices(invoiceServiceTypeDtos);
+
+                Optional<List<InvoiceServiceType>> invoiceServiceTypes = invoiceServiceTypeRepository.findAllByInvoice(invoice.get());
+
+                List<InvoiceServiceTypeDto> invoiceServiceTypeDtos = new ArrayList<>();
+
+                if (invoiceServiceTypes.isPresent()) {
+                    for (InvoiceServiceType invoiceServiceType : invoiceServiceTypes.get()) {
+
+                        InvoiceServiceTypeDto invoiceServiceTypeDto = new InvoiceServiceTypeDto();
+
+                        if (invoiceServiceType.getServiceType().getName() != null) {
+                            invoiceServiceTypeDto.setName(invoiceServiceType.getServiceType().getName());
+                        }
+
+                        if (invoiceServiceType.getServiceType().getPrice() != null) {
+                            invoiceServiceTypeDto.setPrice(invoiceServiceType.getServiceType().getPrice());
+                        }
+
+                        if (invoiceServiceType.getServiceType().getDurationInMonth() != null) {
+                            invoiceServiceTypeDto.setDurationInMonth(invoiceServiceType.getServiceType().getDurationInMonth());
+                        }
+
+                        if (invoiceServiceType.getServiceType().getCategory().getName() != null) {
+                            invoiceServiceTypeDto.setVehicleCategory(invoiceServiceType.getServiceType().getCategory().getName());
+                        }
+
+                        if (invoiceServiceType.getReference() != null) {
+                            invoiceServiceTypeDto.setReference(invoiceServiceType.getReference());
+                        }
+
+                        if (invoiceServiceType.getPaymentDate() != null) {
+                            invoiceServiceTypeDto.setPaymentDate(invoiceServiceType.getPaymentDate());
+                        }
+
+                        if (invoiceServiceType.getServiceType().getDurationInMonth() != null && invoiceServiceType.getPaymentDate() != null) {
+                            invoiceServiceTypeDto.setExpiryDate(invoiceServiceType.getPaymentDate().plusMonths(invoiceServiceType.getServiceType().getDurationInMonth()));
+                        }
+
+                        invoiceServiceTypeDtos.add(invoiceServiceTypeDto);
+                    }
+
+                    verificationDto.setInvoiceServices(invoiceServiceTypeDtos);
+                }
+
+                return verificationDto;
+
             }
-
-            return verificationDto;
-
         }
         return null;
     }
