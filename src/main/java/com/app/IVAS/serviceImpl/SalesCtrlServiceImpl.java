8e52ea37c38db3bd75c3519f38a5f347a74ab1e5
laganch <laganch@yahoo.com>
2023-06-02 08:55:08 +0100

    updated the plate number

--- src/main/java/com/app/IVAS/serviceImpl/SalesCtrlServiceImpl.java
+++ src/main/java/com/app/IVAS/serviceImpl/SalesCtrlServiceImpl.java
@@ -62,7 +62,6 @@ public class SalesCtrlServiceImpl implements SalesCtrlService {
     private final AppRepository appRepository;
 
     @Override
-    @Transactional
     public Invoice SaveSales(SalesDto sales) {
         Vehicle vehicle = new Vehicle();
         Sales sales1 = new Sales();
@@ -574,6 +573,11 @@ public class SalesCtrlServiceImpl implements SalesCtrlService {
         PlateNumberType type = plateNumberTypeRepository.findById(sales.getPlatetype()).get();
 //        InsuranceCompany insuranceCompany = insuranceRepository.findById(sales.getInsurance()).get();
         portalUser = portalUserRepository.findFirstByPhoneNumber(sales.getPhone_number());
+
+        PlateNumber plate = plateNumberRepository.findFirstByPlateNumberIgnoreCase(sales.getPlate());
+        if(plate != null){
+            return null;
+        }
 
         if (portalUser == null) {
             dto.setAddress(sales.getAddress());
